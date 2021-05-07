f174584fe3e05b6758395e04a5491aa2475a9c1d
John Trammell <John.Trammell@target.com>
2021-05-07 15:25:15 -0500

    done with certs, starting server

---
+++ mtls/index.js
@@ -0,0 +1,27 @@
+const https = require('https');
+const fs = require('fs');
+
+const hostname = 'localhost';
+const port = 3000;
+
+
+// "rejectUnauthorized:true" requires the browser to supply a cert signed by
+// the CA before interacting with the server.
+const options = { 
+    ca: fs.readFileSync('ca.crt'), 
+    cert: fs.readFileSync('server.crt'), 
+    key: fs.readFileSync('server.key'), 
+    rejectUnauthorized: true,
+    requestCert: true, 
+}; 
+
+const server = https.createServer(options, (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
