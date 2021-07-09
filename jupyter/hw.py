4aba78bc60d1da99ffec256a1feb0a44eb79526c
John Trammell <John.Trammell@target.com>
2021-07-09 13:48:26 -0500

    simple notebook

---
+++ jupyter/hw.py
@@ -0,0 +1,30 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# ### Hello markdown world
+# 
+# This should **format** _nicely_
+# 
+# [How do links look?](https://target.com/)
+# 
+
+# %%
+msg = "hello world!!!"
+print(msg)
+print(2+2)
+
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 6.3, 100)
+# %%
+1+1
+# %%
+plt.plot(x, np.sin(x))
+plt.show()
+
+
+# %%
+
+
+
