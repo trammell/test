2eaf16e48cdbfaaa9bef460760c41882185d1ca4
John Trammell <John.Trammell@gmail.com>
2024-05-12 22:30:03 -0500

    simple spike

--- speedtest/speedtest.py
+++ speedtest/speedtest.py
@@ -1,12 +1,25 @@
+import argparse
 import datetime
 import os
 import re
 import subprocess
 import time
-from influxdb import InfluxDBClient
+from typing import Tuple
+
+from influxdb_client_3 import InfluxDBClient3, Point
+
+
+
+TOKEN = os.environ["INFLUXDATA_PAT"]
+
+
 
 """
-https://pimylifeup.com/raspberry-pi-internet-speed-monitor/#synctografana
+Relevant documents:
+
+* https://pimylifeup.com/raspberry-pi-internet-speed-monitor/
+* https://pimylifeup.com/raspberry-pi-internet-speed-monitor/#synctografana
+* https://github.com/InfluxCommunity/influxdb3-python
 """
 
 
@@ -29,10 +42,10 @@ def get_results() -> Tuple[float, float, float, float, float]:
     download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE)
     upload = re.search('Upload:\s+(.*?)\s', response, re.MULTILINE)
 
-    latency = 1000.0 * latency.group(1)         # convert to seconds
-    jitter = 1000.0 * jitter.group(1)           # convert to seconds
-    download = 1000000.0 * download.group(1)    # convert to bits/second
-    upload = 1000000.0 * upload.group(1)        # convert to bits/second
+    latency = 0.0001 * float(latency.group(1))         # convert to seconds
+    jitter = 0.0001 * float(jitter.group(1))           # convert to seconds
+    download = 1000000.0 * float(download.group(1))    # convert to bits/second
+    upload = 1000000.0 * float(upload.group(1))        # convert to bits/second
     return (latency, jitter, download, upload, duration)
 
 
@@ -49,11 +62,26 @@ def persist(latency: float, jitter: float, download: float, upload: float, durat
             "upload": upload,
         },
     }
-    client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'pimylifeup', 'internetspeed')
-    client.write_points(speed_data)
+    client = InfluxDBClient3(token=TOKEN,
+                             host="https://us-east-1-1.aws.cloud2.influxdata.com",
+                             org="network",
+                             database="speedtest")
+
+    pt = (Point("measurement").tag("location", "meowmeownet").tag("host", "furby")
+          .field("latency", latency).field("jitter",jitter))
+
+    client.write(pt)
+
+
+
+def _timestamp() -> str:
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 def main():
+    """
+
+    """
     latency, jitter, download, upload, duration = get_results()
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f'{timestamp} {latency=}s {jitter=}s {download=}b/s {upload=}b/s {duration=}s')
@@ -61,6 +89,15 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--dry-run", action="store_true",
+                        help="Don't actually run the speed test (use dummy values).")
+    args = parser.parse_args()
+    if args.dry_run:
+        latency, jitter, download, upload, duration = (0.01, 0.001, 500001, 500002, 1.5)
+        timestamp = _timestamp()
+        print(f'{timestamp} {latency=}s {jitter=}s {download=}b/s {upload=}b/s {duration=}s')
+        persist(latency, jitter, download, upload, duration)
+    else:
+        main()
 
